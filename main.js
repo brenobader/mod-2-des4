e80097b7872bbb73741319b26ca5aa1cc2154734
brenobader <brenobader@gmail.com>
2021-05-31 15:44:20 -0300

    subir o primeiro repositório

---
+++ main.js
@@ -0,0 +1,31 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+//3 - crie uma função que receba seu nome, idade, e estilo musical preferido (parâmetros) e exiba no console
+
+//4 - crie uma função que receba o seu filme favorito, musica favorita (parâmetros) e exiba no console
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+function motivacional(){
+    console.log("Lula 2022")
+}
+motivacional ()
+
+
+function nome(meunome){
+    console.log(`meu nome é ${meunome}`)
+}
+nome(`Breno`)
+
+
+function dados1(nome,idade,estilomusical){
+    console.log(`${nome},${idade},${estilomusical}`)
+}
+dados1 (`Breno`, 26,`modão`)
+
+function dados2(filmefavorito,musicafavorita){
+    console.log(`${filmefavorito},${musicafavorita}`)
+}
+dados2(`Socrates`,`Ideologia`)
